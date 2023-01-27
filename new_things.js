3dbe19d2ce3c9a4a654bd0952e7c1c2e881cb6cb
Damien Redd <damiens.redd@gmail.com>
2023-01-26 22:25:14 -0600

    updated

--- new_things.js
+++ new_things.js
@@ -4,14 +4,15 @@ let daughter = "Ava, "
 let son1 = "Drake, "
 let son2 = "Jack"
 let family = husband + wife + daughter + son1 + son2
-    console.log(family);
 
 // if/else statement, kinda works...
 const parents = husband + wife
 const kids = daughter + son1 + son2
-var group = kids
+var group = parents
 if (group === parents) {
     console.log("Adults: ", wife, husband);
-} else (group === kids); {
-        console.log("Children", daughter, son1, son2);
+} else if (group === kids) {
+        console.log("Children", daughter, son1, son2); 
+} else {
+    console.log(family);
 }
