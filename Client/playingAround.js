2e5bd123270f67667ae0c39d95531e5f82a8ecc4
Damien Redd <damiens.redd@gmail.com>
2023-02-27 09:46:33 -0600

    more work

---
+++ Client/playingAround.js
@@ -0,0 +1,58 @@
+const sanctuaryContainer = document.querySelector('#sancContainer');
+const form = document.querySelector('form');
+const sancAddress = document.querySelector('#loc')
+const sancType = document.querySelector('#type')
+const sancInfo = document.querySelector('')
+
+const baseURL =  axios.post('http://localhost:4004/zombies')
+
+function handleSubmit(e) {
+    e.preventDefault()
+
+    if (sancAddress.value < 1) {
+        alert ('You must enter an address for this sanctuary.')
+        return
+    }
+    if (sancType.value < 1){
+        alert('You must enter the type of building this is.')
+        return
+    }
+    if (sancInfo.value < 1) {
+        alert ('You must enter relevant details about this sanctuary.')
+    }
+    let body = {
+        sancAddress: sancAddress.value,
+        sancType: sancType.value,
+        sancInfo: sancInfo.value,
+    }
+
+    axios.post('http://localhost:4004/zombies', body)
+    .then()
+}
+
+function deleteCard(id) {
+    axios.delete(`http://localhost:4004/zombies/${id}`)
+        .then(() => getSanc())
+        .catch(err => console.log(err))
+}
+
+function getSanc() {
+    sancList.innerHTML = ''
+
+    axios.get('http://localhost:4004/zombies')
+    .then(res => {
+        res.data.forEach(elem => {
+            let sancCard = `<div class="sanc-card">
+            <h2>${elem.address}<h2>
+            <h2${elem.type}<h2>
+            <h3>${elem.info}<h3>
+            <button onclick="deleteCard(${elem['sanc_id']})">Delete</button>
+            </div>`
+            
+            sancList.innerHTML += sancCard
+        })
+    })
+}
+
+getSanc()
+form.addEventListener('submit', handleSubmit)
---
+++ Server/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const {SERVER_PORT} = process.env
+const {seed, getSanc, handleSubmit, deleteCard} = require('./controller.js')
+
+app.use(express.json())
+app.use(cors())
+
+app.post('/seed', seed)
+
+app.get('/sanctuary', getSanc)
+
+app.listen(SERVER_PORT, () => console.log(`up on ${SERVER_PORT}`))
